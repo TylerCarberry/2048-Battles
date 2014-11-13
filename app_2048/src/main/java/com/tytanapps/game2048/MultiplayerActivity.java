85ae053879f35af0e21068d2e373a4661f8ef36b
Tyler Carberry <thetytan34@gmail.com>
2014-11-13 17:23:16 -0500

    Code Cleanup
    Removed unused code from the sample game ButtonClicker2000. Added more
    comments.

--- app_2048/src/main/java/com/tytanapps/game2048/MultiplayerActivity.java
+++ app_2048/src/main/java/com/tytanapps/game2048/MultiplayerActivity.java
@@ -43,7 +43,7 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
         View.OnClickListener, RealTimeMessageReceivedListener,
         RoomStatusUpdateListener, RoomUpdateListener, OnInvitationReceivedListener {
 
-    final static String TAG = "ButtonClicker2000";
+    final static String LOG_TAG = MultiplayerActivity.class.getSimpleName();
 
     // Request codes for the UIs that we show with startActivityForResult:
     final static int RC_SELECT_PLAYERS = 10000;
@@ -100,37 +100,29 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
                 .build();
     }
 
-
-
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.quick_game_button:
-                // play a single-player game
+                // Create a multiplayer game
                 startQuickGame();
                 break;
             case R.id.send_button:
-                // play a single-player game
+                // send a message to the other player
                 sendMessage(getMessage(), false);
                 break;
             case R.id.in_game_button:
                 // play a single-player game
                 Toast.makeText(this, "Is game active? " + (mRoomId != null), Toast.LENGTH_SHORT).show();
-
-
                 if(mParticipants == null)
                     Toast.makeText(this, "mPartic is null", Toast.LENGTH_SHORT).show();
                 else {
                     Toast.makeText(this, "mPartic is not null", Toast.LENGTH_SHORT).show();
                     Toast.makeText(this, "mPartic size: " + mParticipants.size(), Toast.LENGTH_SHORT).show();
-
                 }
                 break;
             case R.id.to_game_button:
-
                 switchToGame();
-
                 break;
         }
     }
@@ -187,7 +179,7 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
                 // we got the result from the "waiting room" UI.
                 if (responseCode == Activity.RESULT_OK) {
                     // ready to start playing
-                    Log.d(TAG, "Starting game (waiting room returned OK).");
+                    Log.d(LOG_TAG, "Starting game (waiting room returned OK).");
                     //startGame(true);
                 } else if (responseCode == GamesActivityResultCodes.RESULT_LEFT_ROOM) {
                     // player indicated that they want to leave the room
@@ -200,7 +192,7 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
                 }
                 break;
             case RC_SIGN_IN:
-                Log.d(TAG, "onActivityResult with requestCode == RC_SIGN_IN, responseCode="
+                Log.d(LOG_TAG, "onActivityResult with requestCode == RC_SIGN_IN, responseCode="
                         + responseCode + ", intent=" + intent);
                 mSignInClicked = false;
                 mResolvingConnectionFailure = false;
@@ -219,16 +211,16 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     // "Invite friends" button. We react by creating a room with those players.
     private void handleSelectPlayersResult(int response, Intent data) {
         if (response != Activity.RESULT_OK) {
-            Log.w(TAG, "*** select players UI cancelled, " + response);
+            Log.w(LOG_TAG, "*** select players UI cancelled, " + response);
             //switchToMainScreen();
             return;
         }
 
-        Log.d(TAG, "Select players UI succeeded.");
+        Log.d(LOG_TAG, "Select players UI succeeded.");
 
         // get the invitee list
         final ArrayList<String> invitees = data.getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
-        Log.d(TAG, "Invitee count: " + invitees.size());
+        Log.d(LOG_TAG, "Invitee count: " + invitees.size());
 
         // get the automatch criteria
         Bundle autoMatchCriteria = null;
@@ -237,11 +229,11 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
         if (minAutoMatchPlayers > 0 || maxAutoMatchPlayers > 0) {
             autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
                     minAutoMatchPlayers, maxAutoMatchPlayers, 0);
-            Log.d(TAG, "Automatch criteria: " + autoMatchCriteria);
+            Log.d(LOG_TAG, "Automatch criteria: " + autoMatchCriteria);
         }
 
         // create the room
-        Log.d(TAG, "Creating room...");
+        Log.d(LOG_TAG, "Creating room...");
         RoomConfig.Builder rtmConfigBuilder = RoomConfig.builder(this);
         rtmConfigBuilder.addPlayersToInvite(invitees);
         rtmConfigBuilder.setMessageReceivedListener(this);
@@ -253,19 +245,19 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
         keepScreenOn();
         //resetGameVars();
         Games.RealTimeMultiplayer.create(mGoogleApiClient, rtmConfigBuilder.build());
-        Log.d(TAG, "Room created, waiting for it to be ready...");
+        Log.d(LOG_TAG, "Room created, waiting for it to be ready...");
     }
 
     // Handle the result of the invitation inbox UI, where the player can pick an invitation
     // to accept. We react by accepting the selected invitation, if any.
     private void handleInvitationInboxResult(int response, Intent data) {
         if (response != Activity.RESULT_OK) {
-            Log.w(TAG, "*** invitation inbox UI cancelled, " + response);
+            Log.w(LOG_TAG, "*** invitation inbox UI cancelled, " + response);
             //switchToMainScreen();
             return;
         }
 
-        Log.d(TAG, "Invitation inbox UI succeeded.");
+        Log.d(LOG_TAG, "Invitation inbox UI succeeded.");
         Invitation inv = data.getExtras().getParcelable(Multiplayer.EXTRA_INVITATION);
 
         // accept invitation
@@ -275,7 +267,7 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     // Accept the given invitation.
     void acceptInviteToRoom(String invId) {
         // accept the invitation
-        Log.d(TAG, "Accepting invitation: " + invId);
+        Log.d(LOG_TAG, "Accepting invitation: " + invId);
         RoomConfig.Builder roomConfigBuilder = RoomConfig.builder(this);
         roomConfigBuilder.setInvitationIdToAccept(invId)
                 .setMessageReceivedListener(this)
@@ -289,7 +281,7 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     // Activity is going to the background. We have to leave the current room.
     @Override
     public void onStop() {
-        Log.d(TAG, "**** got onStop");
+        Log.d(LOG_TAG, "**** got onStop");
 
         // if we're in a room, leave it.
         leaveRoom();
@@ -314,10 +306,10 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     public void onStart() {
         //switchToScreen(R.id.screen_wait);
         if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            Log.w(TAG,
+            Log.w(LOG_TAG,
                     "GameHelper: client was already connected on onStart()");
         } else {
-            Log.d(TAG,"Connecting client.");
+            Log.d(LOG_TAG,"Connecting client.");
             mGoogleApiClient.connect();
         }
         super.onStart();
@@ -335,7 +327,7 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
 
     // Leave the room.
     void leaveRoom() {
-        Log.d(TAG, "Leaving room.");
+        Log.d(LOG_TAG, "Leaving room.");
         //mSecondsLeft = 0;
         stopKeepingScreenOn();
         if (mRoomId != null) {
@@ -393,21 +385,20 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
 
     @Override
     public void onConnected(Bundle connectionHint) {
-        Log.d(TAG, "onConnected() called. Sign in successful!");
-
-        Log.d(TAG, "Sign-in succeeded.");
+        Log.d(LOG_TAG, "onConnected() called. Sign in successful!");
+        Log.d(LOG_TAG, "Sign-in succeeded.");
 
         // register listener so we are notified if we receive an invitation to play
         // while we are in the game
         Games.Invitations.registerInvitationListener(mGoogleApiClient, this);
 
         if (connectionHint != null) {
-            Log.d(TAG, "onConnected: connection hint provided. Checking for invite.");
+            Log.d(LOG_TAG, "onConnected: connection hint provided. Checking for invite.");
             Invitation inv = connectionHint
                     .getParcelable(Multiplayer.EXTRA_INVITATION);
             if (inv != null && inv.getInvitationId() != null) {
                 // retrieve and cache the invitation ID
-                Log.d(TAG,"onConnected: connection hint has a room invite!");
+                Log.d(LOG_TAG,"onConnected: connection hint has a room invite!");
                 acceptInviteToRoom(inv.getInvitationId());
                 return;
             }
@@ -418,16 +409,16 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
 
     @Override
     public void onConnectionSuspended(int i) {
-        Log.d(TAG, "onConnectionSuspended() called. Trying to reconnect.");
+        Log.d(LOG_TAG, "onConnectionSuspended() called. Trying to reconnect.");
         mGoogleApiClient.connect();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.d(TAG, "onConnectionFailed() called, result: " + connectionResult);
+        Log.d(LOG_TAG, "onConnectionFailed() called, result: " + connectionResult);
 
         if (mResolvingConnectionFailure) {
-            Log.d(TAG, "onConnectionFailed() ignoring connection failure; already resolving.");
+            Log.d(LOG_TAG, "onConnectionFailed() ignoring connection failure; already resolving.");
             return;
         }
 
@@ -445,7 +436,7 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     // is connected yet).
     @Override
     public void onConnectedToRoom(Room room) {
-        Log.d(TAG, "onConnectedToRoom.");
+        Log.d(LOG_TAG, "onConnectedToRoom.");
 
         // get room ID, participants and my ID:
         mRoomId = room.getRoomId();
@@ -453,11 +444,11 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
         mMyId = room.getParticipantId(Games.Players.getCurrentPlayerId(mGoogleApiClient));
 
         // print out the list of participants (for debug purposes)
-        Log.d(TAG, "Room ID: " + mRoomId);
-        Log.d(TAG, "My ID " + mMyId);
-        Log.d(TAG, "Is mParticipants null " + (mParticipants == null));
+        Log.d(LOG_TAG, "Room ID: " + mRoomId);
+        Log.d(LOG_TAG, "My ID " + mMyId);
+        Log.d(LOG_TAG, "Is mParticipants null " + (mParticipants == null));
 
-        Log.d(TAG, "<< CONNECTED TO ROOM>>");
+        Log.d(LOG_TAG, "<< CONNECTED TO ROOM>>");
 
         //switchToGame();
     }
@@ -467,7 +458,7 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     @Override
     public void onLeftRoom(int statusCode, String roomId) {
         // we have left the room; return to main screen.
-        Log.d(TAG, "onLeftRoom, code " + statusCode);
+        Log.d(LOG_TAG, "onLeftRoom, code " + statusCode);
         //switchToMainScreen();
     }
 
@@ -487,9 +478,9 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     // Called when room has been created
     @Override
     public void onRoomCreated(int statusCode, Room room) {
-        Log.d(TAG, "onRoomCreated(" + statusCode + ", " + room + ")");
+        Log.d(LOG_TAG, "onRoomCreated(" + statusCode + ", " + room + ")");
         if (statusCode != GamesStatusCodes.STATUS_OK) {
-            Log.e(TAG, "*** Error: onRoomCreated, status " + statusCode);
+            Log.e(LOG_TAG, "*** Error: onRoomCreated, status " + statusCode);
             showGameError();
             return;
         }
@@ -501,9 +492,9 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     // Called when room is fully connected.
     @Override
     public void onRoomConnected(int statusCode, Room room) {
-        Log.d(TAG, "onRoomConnected(" + statusCode + ", " + room + ")");
+        Log.d(LOG_TAG, "onRoomConnected(" + statusCode + ", " + room + ")");
         if (statusCode != GamesStatusCodes.STATUS_OK) {
-            Log.e(TAG, "*** Error: onRoomConnected, status " + statusCode);
+            Log.e(LOG_TAG, "*** Error: onRoomConnected, status " + statusCode);
             showGameError();
             return;
         }
@@ -512,9 +503,9 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
 
     @Override
     public void onJoinedRoom(int statusCode, Room room) {
-        Log.d(TAG, "onJoinedRoom(" + statusCode + ", " + room + ")");
+        Log.d(LOG_TAG, "onJoinedRoom(" + statusCode + ", " + room + ")");
         if (statusCode != GamesStatusCodes.STATUS_OK) {
-            Log.e(TAG, "*** Error: onRoomConnected, status " + statusCode);
+            Log.e(LOG_TAG, "*** Error: onRoomConnected, status " + statusCode);
             showGameError();
             return;
         }
@@ -579,69 +570,27 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
         if (room != null) {
             mParticipants = room.getParticipants();
         }
-
-
-        //Toast.makeText(this, "In update room, num participants = " + (mParticipants.size()), Toast.LENGTH_SHORT).show();
-
-
-
-        //if (mParticipants != null) {
-         //   updatePeerScoresDisplay();
-        //}
     }
 
     @Override
     public void onRealTimeMessageReceived(RealTimeMessage rtm) {
         //Toast.makeText(this, "Message Received", Toast.LENGTH_LONG).show();
-
+        
         byte[] buf = rtm.getMessageData();
 
+        // Convert the message to a string
         String message = "";
-
         for(byte b : buf) {
             char letter = (char) b;
             message += letter;
         }
 
+        // Messages sent with the score start with s followed by the score (s500)
         if(message.charAt(0) == 's')
             updateOpponentTextView(Integer.parseInt(message.substring(1)));
+        // The other user sent a message from the edit text field
         else
             Toast.makeText(this, message , Toast.LENGTH_LONG).show();
-
-
-        /*
-
-        byte[] buf = rtm.getMessageData();
-        String sender = rtm.getSenderParticipantId();
-        Log.d(TAG, "Message received: " + (char) buf[0] + "/" + (int) buf[1]);
-
-        if (buf[0] == 'F' || buf[0] == 'U') {
-            // score update.
-            int existingScore = mParticipantScore.containsKey(sender) ?
-                    mParticipantScore.get(sender) : 0;
-            int thisScore = (int) buf[1];
-            if (thisScore > existingScore) {
-                // this check is necessary because packets may arrive out of
-                // order, so we
-                // should only ever consider the highest score we received, as
-                // we know in our
-                // game there is no way to lose points. If there was a way to
-                // lose points,
-                // we'd have to add a "serial number" to the packet.
-                mParticipantScore.put(sender, thisScore);
-            }
-
-            // update the scores on the screen
-            updatePeerScoresDisplay();
-
-            // if it's a final score, mark this participant as having finished
-            // the game
-            if ((char) buf[0] == 'F') {
-                mFinishedParticipants.add(rtm.getSenderParticipantId());
-            }
-        }
-
-        */
     }
 
     private void updateOpponentTextView(int score) {
@@ -654,55 +603,33 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
         return messageTextview.getText().toString();
     }
 
-    // Broadcast my score to everybody else.
-    void sendMessage(String message, boolean reliable) {
+    /**
+     * Broadcast a message to the other players
+     * @param message The message to send
+     * @param reliable Whether or not the sent message will be reliable
+      */
+    protected void sendMessage(String message, boolean reliable) {
 
         if(mRoomId == null) {
             Toast.makeText(this, "You are not in a game", Toast.LENGTH_SHORT).show();
             return;
-
         }
 
-        //Toast.makeText(this, "SEND MESSAGE", Toast.LENGTH_SHORT).show();
-        //Toast.makeText(this, "Is mParticipants null in send message " + (mParticipants == null), Toast.LENGTH_SHORT).show();
-        //Toast.makeText(this, "STEP 2", Toast.LENGTH_SHORT).show();
-        // // First byte in message indicates whether it's a final score or not
-
+        // Convert the string message to an array of bytes
         mMsgBuf = new byte[message.length()];
-
-        for(int position = 0; position < message.length(); position++) {
+        for(int position = 0; position < message.length(); position++)
             mMsgBuf[position] = (byte) message.charAt(position);
-        }
-
-        //mMsgBuf[0] = (byte) (1);
-
-        // Second byte is the score.
-        //mMsgBuf[1] = (byte) 'B';
 
         // Send to every other participant.
         for (Participant p : mParticipants) {
-
-            Log.d(TAG, "This will be printed for every participant receiving the message");
-
-
-            //if (p.getParticipantId().equals(mMyId))
-            //    continue;
-           // if (p.getStatus() != Participant.STATUS_JOINED)
-            //    continue;
-            if (reliable) {
-
-                //Toast.makeText(this, "RELIABLE MESSAGE SENT", Toast.LENGTH_SHORT).show();
-
-                // final score notification must be sent via reliable message
-                Games.RealTimeMultiplayer.sendReliableMessage(mGoogleApiClient, null, mMsgBuf,
-                        mRoomId, p.getParticipantId());
-            } else {
-
-                //Toast.makeText(this, "UNRELIABLE MESSAGE SENT", Toast.LENGTH_SHORT).show();
-
-                // it's an interim score notification, so we can use unreliable
-                Games.RealTimeMultiplayer.sendUnreliableMessage(mGoogleApiClient, mMsgBuf, mRoomId,
-                        p.getParticipantId());
+            // Only send to joined players other than yourself
+            if (!p.getParticipantId().equals(mMyId) && (p.getStatus() == Participant.STATUS_JOINED)) {
+                if (reliable)
+                    Games.RealTimeMultiplayer.sendReliableMessage(mGoogleApiClient, null, mMsgBuf,
+                          mRoomId, p.getParticipantId());
+                else
+                    Games.RealTimeMultiplayer.sendUnreliableMessage(mGoogleApiClient, mMsgBuf, mRoomId,
+                            p.getParticipantId());
             }
         }
     }
@@ -720,14 +647,10 @@ public class MultiplayerActivity extends BaseGameActivity implements GoogleApiCl
     }
 
     @Override
-    public void onSignInFailed() {
-
-    }
+    public void onSignInFailed() {    }
 
     @Override
-    public void onSignInSucceeded() {
-
-    }
+    public void onSignInSucceeded() {    }
 
     /**
      * A placeholder fragment containing a simple view.
