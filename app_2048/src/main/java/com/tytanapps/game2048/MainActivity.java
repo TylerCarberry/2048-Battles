7a9a80cc6af551f7efb75278c6f8fbc0ce0be1f8
Tyler Carberry <thetytan34@gmail.com>
2014-12-10 17:53:35 -0500

    Created new MainActivity
    The app logo is on top. Below that is the single player button which
    switches to SelectModeActivity (the old MainActivity) when pressed. On
    the button of the screen is the multiplayer game button. This starts a
    new multiplayer quick game immediately.

---
+++ app_2048/src/main/java/com/tytanapps/game2048/MainActivity.java
@@ -0,0 +1,78 @@
+package com.tytanapps.game2048;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.single_player_imagebutton:
+                startActivity(new Intent(this, SelectModeActivity.class));
+                break;
+            case R.id.multiplayer_imagebutton:
+                Intent multiplayerIntent = new Intent(getBaseContext(), MultiplayerActivity.class);
+                multiplayerIntent.putExtra("startMultiplayer", true);
+                startActivity(multiplayerIntent);
+                break;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
