bf1595652ad4a7bd1591881513436b0748d4a0a1
Tyler Carberry <thetytan34@gmail.com>
2015-01-19 10:59:05 -0500

    Notification and action bar are hidden
    In all activities other than SettingActivity
    
    Closes #125 Remove the action bar from GameActivity
    Closes #101 Make app full screen

--- app_2048/src/main/java/com/tytanapps/game2048/MainActivity.java
+++ app_2048/src/main/java/com/tytanapps/game2048/MainActivity.java
@@ -20,8 +20,6 @@ import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -85,28 +83,6 @@ public class MainActivity extends BaseGameActivity implements QuestUpdateListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
