9eb555482a1cb6bcb6f9bb16ad714a3edfffd31b
Tyler Carberry <thetytan34@gmail.com>
2015-02-01 13:00:51 -0500

    The custom game preview appears immediately
    The 4x4 game grid appears immediately when CustomGameActivity is
    loaded. Previously you needed to make a change to the game before the
    preview would appear.
    
    Closes #135 Show custom game preview immediately

--- app_2048/src/main/java/com/tytanapps/game2048/CustomGameActivity.java
+++ app_2048/src/main/java/com/tytanapps/game2048/CustomGameActivity.java
@@ -8,8 +8,6 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -42,25 +40,11 @@ public class CustomGameActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_custom_game, menu);
-        return true;
-    }
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if(hasFocus)
+            updateGamePreview();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        super.onWindowFocusChanged(hasFocus);
     }
 
     public void onClick(View view) {
